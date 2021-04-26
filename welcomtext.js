bad2c89fd02ad2010644b61724a411af69e1dd6e
Tdestroyer25 <83205806+Tdestroyer25@users.noreply.github.com>
2021-04-26 03:57:57 -0400

    Add files via upload

---
+++ welcomtext.js
@@ -0,0 +1,9 @@
+const welcome = (number, groupname) => {
+    return `Hola @${number}. Bienvenid@ a nuentro grupo ${groupname} Aquí aprenderas parte de nuestro conocimiento. Mantente activ@ 24/7`
+}
+exports.welcome = welcome
+
+const bye = (number) => {
+    return `YA era hora de que te largues pinche perucul@ @${number}. Sayōnara BAKA!👋`
+}
+exports.bye = bye
